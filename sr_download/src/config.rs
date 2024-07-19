8a7c18f5ee5e35a93af74ed921d609c31d0e5d4f
shenjack <3695888@qq.com>
2024-07-20 00:36:11 +0800

    处理一下数据库

--- sr_download/src/config.rs
+++ sr_download/src/config.rs
@@ -1,9 +1,38 @@
-use serde::Deserialize;
+use std::path::Path;
 
-#[derive(Deserialize)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub struct ConfigFile {
     pub db_url: String,
+    pub db_schema: String,
     pub max_connections: u32,
     pub sqlx_logging: bool,
-    pub schema_search_path: String,
+}
+
+impl Default for ConfigFile {
+    fn default() -> Self {
+        Self {
+            db_url: "postgres://srdown:srdown@192.168.3.22:10001/srdown?currentSchema=srdown"
+                .to_string(),
+            db_schema: "srdown".to_string(),
+            max_connections: 10,
+            sqlx_logging: true,
+        }
+    }
+}
+
+impl ConfigFile {
+    pub fn read_from_file(file_path: &Path) -> anyhow::Result<Self> {
+        let data = std::fs::read_to_string(file_path)?;
+        let config: ConfigFile = toml::from_str(&data)?;
+        Ok(config)
+    }
+
+    pub fn write_default_to_file(file_path: &Path) -> anyhow::Result<()> {
+        let config = ConfigFile::default();
+        let toml = toml::to_string(&config)?;
+        std::fs::write(file_path, toml)?;
+        Ok(())
+    }
 }

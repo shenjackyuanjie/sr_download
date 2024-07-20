6365044cf9b9c5c53d1297a7fdd1185349aad572
shenjack <3695888@qq.com>
2024-07-20 14:28:48 +0800

    还行

--- sr_download/src/main.rs
+++ sr_download/src/main.rs
@@ -1,5 +1,9 @@
 mod config;
 mod db;
+mod model;
+mod net;
+
+pub type SaveId = u32;
 
 #[tokio::main]
 async fn main() {}
--- sr_download/src/net.rs
+++ sr_download/src/net.rs
@@ -1,50 +1,205 @@
-// use Request::blocking::Client;
+use reqwest::Client;
+use std::time::Duration;
+
+use crate::SaveId;
 
 pub struct Downloader {
     pub client: Client,
+    timeout: Duration,
+}
+
+/// 使用 any 下载下来的文件
+pub enum SaveFile {
+    /// 是艘船
+    Ship(String),
+    /// 是存档
+    Save(String),
+}
+
+impl SaveFile {
+    pub fn as_ship(&self) -> Option<&str> {
+        match self {
+            SaveFile::Ship(s) => Some(s),
+            _ => None,
+        }
+    }
+
+    pub fn as_save(&self) -> Option<&str> {
+        match self {
+            SaveFile::Save(s) => Some(s),
+            _ => None,
+        }
+    }
+
+    pub fn is_ship(&self) -> bool {
+        self.as_ship().is_some()
+    }
+
+    pub fn is_save(&self) -> bool {
+        self.as_save().is_some()
+    }
 }
 
 impl Downloader {
-    pub fn new() -> Self {
+    pub fn new(timeout: Duration) -> Self {
         Self {
             client: Client::new(),
+            timeout,
         }
     }
-}
 
-fn list_file_in_dir(dir: &str, file_vec: &mut Vec<u64>) -> Result<(), Box<dyn std::error::Error>> {
-    let save_paths = std::fs::read_dir(dir)?;
-    for save_path in save_paths {
-        if let Ok(save_path) = save_path {
-            let file_name = save_path.file_name();
-            let file_name = file_name.to_str().unwrap();
-            // 判断文件名是否以 .xml 结尾
-            if file_name.ends_with(".xml") {
-                let file_id = file_name.trim_end_matches(".xml");
-                let file_id = file_id.parse::<u64>()?;
-                file_vec.push(file_id);
+    pub fn as_ship_url(id: SaveId) -> String {
+        format!(
+            "http://jundroo.com/service/SimpleRockets/DownloadRocket?id={}",
+            id
+        )
+    }
+
+    pub fn as_save_url(id: SaveId) -> String {
+        format!(
+            "http://jundroo.com/service/SimpleRockets/DownloadSandBox?id={}",
+            id
+        )
+    }
+
+    /// 尝试用 ship 或者 save 的 API 下载文件
+    /// 如果两个都没下载到，返回 None
+    /// 如果下载到了，返回 Some(文件内容)
+    pub async fn try_download_as_any(&self, id: SaveId) -> Option<SaveFile> {
+        // 先尝试用 ship 的 API 下载
+        let ship_url = Self::as_ship_url(id);
+        let ship_try = self.client.get(&ship_url).send().await;
+        if let Ok(ship_try) = ship_try {
+            if ship_try.status().is_success() {
+                if let Ok(body) = ship_try.text().await {
+                    // 再判空
+                    if !body.is_empty() || body == "0" {
+                        return Some(SaveFile::Ship(body));
+                    }
+                }
             }
         }
+        // 否则尝试用 save 的 API 下载
+        let save_url = Self::as_save_url(id);
+        let save_try = self.client.get(&save_url).send().await;
+        if let Ok(save_try) = save_try {
+            if save_try.status().is_success() {
+                if let Ok(body) = save_try.text().await {
+                    // 再判空
+                    if !body.is_empty() || body == "0" {
+                        return Some(SaveFile::Save(body));
+                    }
+                }
+            }
+        }
+        None
     }
-    file_vec.sort();
-    Ok(())
+
+    /// 尝试用 ship 的 API 下载文件
+    pub async fn download_as_ship(&self, id: SaveId) -> Option<String> {
+        let url = Self::as_ship_url(id);
+        let try_res = self.client.get(&url).send().await;
+        if let Ok(try_res) = try_res {
+            if try_res.status().is_success() {
+                if let Ok(body) = try_res.text().await {
+                    if !body.is_empty() || body == "0" {
+                        return Some(body);
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    /// 尝试用 save 的 API 下载文件
+    pub async fn download_as_save(&self, id: SaveId) -> Option<String> {
+        let url = Self::as_save_url(id);
+        let try_res = self.client.get(&url).send().await;
+        if let Ok(try_res) = try_res {
+            if try_res.status().is_success() {
+                if let Ok(body) = try_res.text().await {
+                    if !body.is_empty() || body == "0" {
+                        return Some(body);
+                    }
+                }
+            }
+        }
+        None
+    }
 }
 
-enum FileKind {
-    Ship,
-    Save,
+impl Default for Downloader {
+    fn default() -> Self {
+        Self::new(Duration::from_secs(1))
+    }
 }
 
-//  飞船 API http://jundroo.com/service/SimpleRockets/DownloadRocket?id=
-//  存档 API http://jundroo.com/service/SimpleRockets/DownloadSandBox?id=
-//  curl http://jundroo.com/service/SimpleRockets/DownloadRocket?id=144444
-// fn get_file_from_jundroo(id: u64) -> Result<FileKind, Box<dyn std::error::Error>> {
-//     let ship_try = reqwest::blocking::get(format!(
-//         "http://jundroo.com/service/SimpleRockets/DownloadRocket?id={}",
-//         id
-//     ))?;
-//     println!("ship_try: {:?}", ship_try);
-//     println!("body: {}", ship_try.text()?);
-
-//     Ok(FileKind::Ship)
+// fn list_file_in_dir(dir: &str, file_vec: &mut Vec<u64>) -> Result<(), Box<dyn std::error::Error>> {
+//     let save_paths = std::fs::read_dir(dir)?;
+//     for save_path in save_paths {
+//         if let Ok(save_path) = save_path {
+//             let file_name = save_path.file_name();
+//             let file_name = file_name.to_str().unwrap();
+//             // 判断文件名是否以 .xml 结尾
+//             if file_name.ends_with(".xml") {
+//                 let file_id = file_name.trim_end_matches(".xml");
+//                 let file_id = file_id.parse::<u64>()?;
+//                 file_vec.push(file_id);
+//             }
+//         }
+//     }
+//     file_vec.sort();
+//     Ok(())
 // }
+
+// enum FileKind {
+//     Ship,
+//     Save,
+// }
+
+// //  飞船 API http://jundroo.com/service/SimpleRockets/DownloadRocket?id=
+// //  存档 API http://jundroo.com/service/SimpleRockets/DownloadSandBox?id=
+// //  curl http://jundroo.com/service/SimpleRockets/DownloadRocket?id=144444
+// // fn get_file_from_jundroo(id: u64) -> Result<FileKind, Box<dyn std::error::Error>> {
+// //     let ship_try = reqwest::blocking::get(format!(
+// //         "http://jundroo.com/service/SimpleRockets/DownloadRocket?id={}",
+// //         id
+// //     ))?;
+// //     println!("ship_try: {:?}", ship_try);
+// //     println!("body: {}", ship_try.text()?);
+
+// //     Ok(FileKind::Ship)
+// // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SHIP_144444: &str = r#"<Ship currentStage="0" throttle="0.000000" liftedOff="0">
+    <Parts>
+        <Part partType="pod-1" id="1" x="-4.000000" y="3.250000" angle="0.000000" angleV="0.000000" activated="0" exploded="0"/>
+    </Parts>
+    <Connections/>
+    <Staging/>
+</Ship>
+"#;
+
+    #[tokio::test]
+    async fn any_as_ship_download_test() {
+        let downloader = Downloader::default();
+        let body = downloader.try_download_as_any(144444).await;
+        assert!(body.is_some());
+        let body = body.unwrap();
+        assert!(body.is_ship());
+        assert_eq!(body.as_ship().unwrap(), SHIP_144444);
+    }
+
+    #[tokio::test]
+    async fn ship_download_test() {
+        let downloader = Downloader::default();
+        let body = downloader.download_as_ship(144444).await;
+        assert!(body.is_some());
+        let body = body.unwrap();
+        assert_eq!(body, SHIP_144444);
+    }
+}
